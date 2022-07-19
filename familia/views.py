7ffb3366d4d413c35d5fc3760904f9f005166027
Kevin Sapia <kevinsapia@hotmail.com>
2022-07-19 14:59:26 -0300

    Trabajo terminado!

--- familia/views.py
+++ familia/views.py
@@ -1,36 +1,30 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.template import loader
 from familia.models import Familiar
 # Create your views here.
 
-
-def agregarfamiliar(self, nom, parent, ed, fechaDeNac, trabaj):
+#ésta funsión agarra los parámetros y los usa para crear un pariente usando la clase Familiar.
+#luego verifica el valor proporcionado por la funsión "existefamiliar"
+#y en base a eso guarda o no el pariente en la base de datos. en ambos casos muestra un render diferente.
+def agregarfamiliar(request, nom, parent, ed, fechaDeNac, trabaj):
     pariente = Familiar(nombre = nom, parentezco = parent, edad= ed, fechaDeNacimiento = fechaDeNac, trabajador = trabaj)
-    pariente.save()
-    #lista = [nom,parent,ed,fechaDeNac,trabaj]
-    #lista = pariente.objects.all
+    if not existefamiliar(pariente.nombre):
+        pariente.save()
+        return render(request,"agregarfamiliar.html", {"nuevofamiliar": pariente})
+    return render(request,"familiarexistente.html",{"nuevofamiliar": pariente})
 
-    #return render(self,"agregarfamiliar.html",{"nuevofamiliar": lista})
-
-    return HttpResponse(f"""
-    <h1>Agregando familiar: </h1>
-    <p>Agregando a la base de datos nombre: {pariente.nombre}. </p>
-    <p>parentezco: {pariente.parentezco}. </p>
-    <p>edad: {pariente.edad}. </p>
-    <p>fechaDeNacimiento: {pariente.fechaDeNacimiento}. </p>
-    <p>trabaja: {pariente.trabajador}.</p>
-    <p>{pariente.nombre} AGREGADO CORRECTAMENTE. </p>
-    """)
-
+#Cree una funsión que revisa si ya existe el nombre en la base de datos y devuelve un valor booleano
+def existefamiliar(nombre):
+    return Familiar.objects.filter(nombre=nombre).exists()
+#funsión utilizada para listar los objetos dentro de la base de datos, de modo que puedan convertirse facilmente en un diccionario 
+#para luego pasarselo al render. el template muestra las entradas guardadas en la base de datos.
 def listarfamiliares(self):
     lista = Familiar.objects.all
 
     return render(self,"lista_familiares.html",{"lista_familiares": lista})
-
+#simplemente muestra la pagina principal.
 def inicio(request):
     
-    return HttpResponse(f"""
-    <h1>Esto es un precario inicio.</h1>
-    """)
+    return render(request,"inicio.html",{})
+
 
